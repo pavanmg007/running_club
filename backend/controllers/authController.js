46bfef5e2e4bec77db83e0c236fa0963f061e50f
Pavan MG <contact.pavanmg@gmail.com>
2025-03-27 16:21:47 +0530

    bcrypt to bcryptjs

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,6 @@
 const User = require('../models/user');
 const Invitation = require('../models/invitation');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('../config/env');
 const pool = require('../config/database');

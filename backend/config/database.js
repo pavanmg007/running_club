99850d59a3870a689b96602f23e1580eb066ecfc
Pavan MG <contact.pavanmg@gmail.com>
2025-03-28 19:08:56 +0530

    logs added

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,9 @@
 const { Pool } = require('pg');
 const { DATABASE_URL } = require('./env');
 const fs = require('fs');
-const caCert = fs.readFileSync('./ca.pem').toString(); // Replace './ca.pem' with the actual path to your downloaded certificate
-
+const caCert = fs.readFileSync('./ca.pem').toString();
+console.log('DATABASE_URL', DATABASE_URL);
+console.log('caCert', caCert);
 const pool = new Pool({
   connectionString: DATABASE_URL,
   ssl: {
